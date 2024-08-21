2106aaa78a12c086936a9c00a932bef847d42044
Pedro Henrique <Pedrohenrique16870@gmail.com>
2024-08-21 14:04:39 -0300

    login

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect
-from database import Produtos, session
+from flask import Flask, render_template, request, redirect, session as flask_session, url_for
+from database import User, Produtos, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def index ():
@@ -22,9 +25,50 @@ def filtrar_categoria():
         produtos_filtrados = session.query(Produtos).filter(Produtos.categoria == categoria_desejada).all()
     else:
         produtos_filtrados = []
-
     session.commit()
     return render_template('index.html', produtos=produtos_filtrados)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, password=hashed_password)
+        session.add(new_user)
+        session.commit()
+        return flask_session('/login')
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = session.query(User).filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            flask_session['user_id'] = user.id
+            return redirect('/one')
+        else:
+            error = "Login falhou! Verifique suas credenciais ou "
+            register_link = "<a href='/register'>registre-se aqui</a>."
+            return render_template('login.html', error=error + register_link)
+
+    return render_template('login.html')
+@app.route('/one')
+def one():
+    if 'user_id' not in flask_session:  # Verifica corretamente a sessão do Flask
+        return redirect('/login')
+    user_id = flask_session['user_id']
+    con = session.query(Produtos).filter_by(user_id=user_id).all()
+    produtos = session.query(Produtos).all()
+    return render_template('index2chefe.html', produtos=produtos, con=con)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,9 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base, sessionmaker  
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 engine = create_engine('sqlite:///loja.db', echo=True)
-
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+    produtos = relationship('Produtos', back_populates='user')  # Corrigido 'con' para 'produtos'
+    
+
 class Produtos(Base):
     __tablename__ = 'produtos'
     id = Column(Integer, primary_key=True)
@@ -14,7 +23,10 @@ class Produtos(Base):
     lucro = Column(Float)
     qtn = Column(Integer)
     categoria = Column(String(100))
-
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='produtos')  # Mantido o nome 'user'
 
 Session = sessionmaker(bind=engine)
 session = Session()
+#Base.metadata.create_all(engine)
+
